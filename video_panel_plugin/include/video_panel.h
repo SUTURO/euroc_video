68cd541299b6f9ba88f3bca018f5b46efeea3080
bennypi <bennypi@tzi.de>
2015-04-24 15:50:07 +0200

    labels, buttons and connections

--- video_panel_plugin/include/video_panel.h
+++ video_panel_plugin/include/video_panel.h
@@ -4,6 +4,8 @@
 #ifndef Q_MOC_RUN
 # include <ros/ros.h>
 # include <rviz/panel.h>
+#include <QLabel>
+#include <QPushButton>
 #endif
 
 namespace video_panel_plugin
@@ -17,7 +19,19 @@ public:
 	VideoPanel(QWidget* parent = 0);
 
 	// ~VideoPanel();
+
+private Q_SLOTS:
+    void handle_button1();
+    void handle_button2();
+    void handle_button3();
 	
+private:
+    QLabel *label1;
+    QLabel *label2;
+    QLabel *label3;
+    QPushButton *button1;
+    QPushButton *button2;
+    QPushButton *button3;
 };
 }
 #endif // VIDEO_PANEL_H
--- video_panel_plugin/src/video_panel.cpp
+++ video_panel_plugin/src/video_panel.cpp
@@ -1,13 +1,54 @@
 #include "video_panel.h"
 #include <stdio.h>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 namespace video_panel_plugin
 {
 	VideoPanel::VideoPanel( QWidget* parent)
 	: rviz::Panel(parent)
 	{
+        QVBoxLayout* vertical_layout = new QVBoxLayout;
 
+        QHBoxLayout* label_layout = new QHBoxLayout;
+        vertical_layout->addLayout(label_layout);
+        label1 = new QLabel("Blub1");
+        label2 = new QLabel("Blub2");
+        label3 = new QLabel("Blub3");
+        label_layout->addWidget(label1);
+        label_layout->addWidget(label2);
+        label_layout->addWidget(label3);
+
+        QHBoxLayout* button_layout = new QHBoxLayout;
+        vertical_layout->addLayout(button_layout);
+        button1 = new QPushButton("Blub1", this);
+        button2 = new QPushButton("Blub2", this);
+        button3 = new QPushButton("Blub3", this);
+        button_layout->addWidget(button1);
+        button_layout->addWidget(button2);
+        button_layout->addWidget(button3);
+
+        connect(button1, SIGNAL (clicked()), this, SLOT (handle_button1()));
+        connect(button2, SIGNAL (clicked()), this, SLOT (handle_button2()));
+        connect(button3, SIGNAL (clicked()), this, SLOT (handle_button3()));
+
+        setLayout(vertical_layout);
 	}
+
+    void VideoPanel::handle_button1()
+    {
+        label1->setText("pushed");
+    }
+
+    void VideoPanel::handle_button2()
+    {
+        label2->setText("pushed");
+    }
+
+    void VideoPanel::handle_button3()
+    {
+        label3->setText("pushed");
+    }
 }
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(video_panel_plugin::VideoPanel,rviz::Panel )
