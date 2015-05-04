08f1871fa923f5d99494a537a527ccb8c34c1c33
Jannik Buckelo <jannikbu@tzi.de>
2015-05-04 14:01:09 +0200

    Worked on timing

--- mongodb_play/src/tf_player.cpp
+++ mongodb_play/src/tf_player.cpp
@@ -17,12 +17,30 @@ TFPlayer::TFPlayer(ros::NodeHandle nh, std::string topic, std::string db_address
 
 void TFPlayer::play(ros::Time start_time, ros::Time end_time)
 {
+  ros::Time time_zero = ros::Time::now();
+  ros::Duration time_offset;
+  bool offset_is_set = false;
   tf2_msgs::TFMessage msg;
   auto_ptr<DBClientCursor> cursor = conn_.query(db_coll_, BSONObj());
   while (cursor->more())
   {
     BSONObj p = cursor->next();
     vector<BSONElement> db_transforms = p["transforms"].Array();
+    Date_t date_db = p.getField("__recorded").Date();
+    ros::Time t = ros::Time();
+    t.fromNSec(date_db.millis);
+
+    if (!offset_is_set)
+    {
+      time_offset = time_zero - t;
+      offset_is_set = true;
+      cout << "db time in nsecs: " << t.nsec << endl;
+      cout << "ros time in nsecs: " << time_zero.nsec << endl;
+      cout << "offset : " << time_offset.nsec << endl;
+    }
+
+    ros::Time::sleepUntil(t + time_offset);
+
     for (vector<BSONElement>::iterator it = db_transforms.begin(); it != db_transforms.end(); ++it)
     {
       geometry_msgs::TransformStamped transformStamped;
@@ -32,8 +50,8 @@ void TFPlayer::play(ros::Time start_time, ros::Time end_time)
       transformStamped.child_frame_id = it->Obj().getField("child_frame_id").toString();
 
       msg.transforms.push_back(transformStamped);
-      cout << msg << endl;
     }
+    //cout << msg << endl;
 
     pub_.publish(msg);
     seq_++;
